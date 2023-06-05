8624a1888cbf7491791b3aa48200c8edeb015016
PurvaTheDataEngineer <purvajavale@gmail.com>
2023-06-05 13:05:38 -0400

    BMI Calculator

---
+++ BMICalculator.py
@@ -0,0 +1,37 @@
+
+# Under 18.5 Underweight	Minimal
+# 18.5 - 24.9 Normal Weight Minimal
+# 25 - 29.9	Overweight Increased
+# 30 - 34.9	Obese High
+# 35 - 39.9	Severely Obese Very High
+# 40 and over Morbidly Obese Extremely High
+
+
+def BMI():
+    name = input("Enter you name: ")
+    weight = int(input("Enter your weight in pounds: "))
+    height = int(input("Enter your height in inches: "))
+
+    BMI = (weight * 703) / (height * height)
+    print(BMI)
+
+    if BMI > 0:
+        if BMI < 18.5:
+            print(name + ", you are underweight.")
+        elif BMI <= 24.9:
+            print(name + ", you are normal weight.")
+        elif BMI < 29.9:
+            print(
+                name + ", you are overweight. You need to exercise more and stop sitting and writing so many python tutorials.")
+        elif BMI < 34.9:
+            print(name + ", you are obese.")
+        elif BMI < 39.9:
+            print(name + ", you are severely obese.")
+        else:
+            print(name + ", you are morbidly obese.")
+    else:
+        print("Enter valid input")
+
+
+if __name__ == '__main__':
+    BMI()
